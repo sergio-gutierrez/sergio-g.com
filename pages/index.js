a0d942e72a552cab4c035089bf3c1b0d22b3a366
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2025-02-13 17:36:11 -0500

    added buttons, changed some formatting

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,26 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>sergio-g.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <div className='top-bar'>
+          <span className="name">Sergio G.</span>
+          <div className="button-container">
+            <button className="button">Home</button>
+            <button className="button">Projects</button>
+            <button className="button">Contact Me</button>
+          </div>
+        </div>
+        
+        <Header title="Hello and Welcome!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+
       </main>
 
       <Footer />
