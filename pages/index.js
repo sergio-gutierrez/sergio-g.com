a5db36c1017d0b6f5b6599d5c09d157041fa8123
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2025-02-17 21:27:30 -0500

    added button and more
    Added a button for CV
    added bio and photo (temp photo)

--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,29 @@ export default function Home() {
 
       <main>
         <div className='top-bar'>
-          <div className='name'>Sergio Gutierrez</div>
+          <div className='name'>Sergio G.</div>
           <div className="button-container">
             <button className="button">Home</button>
             <button className="button">Projects</button>
-            <button className="button">Contact Me</button>
+            <button className="button">Contact</button>
+            <button className="button">CV</button>
           </div>
         </div>
-        
-        <Header title="Hello and Welcome!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+
+        <Header title="Sergio Gutierrez" />
+
+        <div className='selfie-container'>
+          <img 
+            src='temp_picture.png'
+            alt='Sergio Gutierrez'
+            className='circle-photo'
+            />
+        </div>
+
+        <p className="bio">
+          My name is Sergio Gutierrez! <br/>
+          I am a Computer Science student at SUNY Oneonta!<br/>
+          Feel free to reach out or check out my projects!
         </p>
       </main>
 
