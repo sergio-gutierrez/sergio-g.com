502e7dc4a47609e9577cdd4053aef13e2fbc75a9
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2025-06-24 16:46:55 -0400

    Update contactPage.js
    edited contact page

--- pages/contactPage.js
+++ pages/contactPage.js
@@ -1,7 +1,8 @@
 const renderContactContent = () => (
     <div className="content-container">
-      <h2>Contact</h2>
-      <p>Contact me at: example@example.com</p>
+      <h2>Contact Me!</h2>
+      <p>Email: gutierrez.d.sergio@gmail.com</p>
+      <p>LinkedIn: www.linkedin.com/in/sergio-gutierrez-305665195</p>
     </div>
   );
 
