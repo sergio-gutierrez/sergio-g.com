42035f6fdd273ccb403a8a471c2e1f027c701c7d
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2025-06-24 12:28:58 -0400

    Update index.js
    updated dependencies

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,18 @@ export default function Home() {
       <main>
         <div className='top-bar'>
           <div className='name'>Sergio G.</div>
-          {/*
+        
           <div className="button-container">
             <button className="button">Home</button>
             <button className="button">Projects</button>
             <button className="button">Contact</button>
             <button className="button">CV</button>
           </div>
-          */}
+          
         </div>
         
 
-{/*
+
         <div className='selfie-container'>
           <img 
             src='temp_picture.png'
@@ -32,16 +32,14 @@ export default function Home() {
             className='circle-photo'
             />
         </div>
-*/}
         {/*<Header title="Sergio Gutierrez" />*/}
-        {/*
+        
         <p className="bio">
           Hello and Welcome to my website! <br/>
           My name is Sergio Gutierrez! <br/>
           I am a Computer Science student at SUNY Oneonta!<br/>
           Feel free to reach out or check out my projects!
         </p>
-        */}
       </main>
 
     </div>
